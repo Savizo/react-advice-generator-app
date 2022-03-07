387b3b2929d24c9f153fa3a95b17ca549b44a175
Aleksandar Georgiev <alekskg1993@gmail.com>
2022-03-07 19:36:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import dice from './images/icon-dice.svg';
+import pauseDesktop from './images/pattern-divider-desktop.svg';
+import pauseMobile from './images/pattern-divider-mobile.svg';
+
+function App() {
+  const [text, setText] = useState([]);
+
+  const fetchAdvice = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+
+    setText(data);
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return (
+    <div className='container'>
+      <h1>Advice #{text.slip.id}</h1>
+      <p>{text.slip.advice}</p>
+
+      <picture>
+        <source media='(min-width: 768px)' srcSet={pauseDesktop} />
+        <img src={pauseMobile} alt='' />
+      </picture>
+
+      <div>
+        <button onClick={fetchAdvice}>
+          <img src={dice} alt='' />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
